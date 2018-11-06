99ed6bac851e6d7bcc0755665c25e9183d9f174a
Gyejoon <00700mm@gmail.com>
2018-11-06 23:50:35 +0900

    Initialized Commit.

---
+++ servey-frontend/src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        hello Servey
+      </div>
+    );
+  }
+}
+
+export default App;

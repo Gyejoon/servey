0985238d2e723c4a37d823aeca22a7d4c1adc6ab
Jun Young Kang <00700mm@gmail.com>
2019-12-16 01:44:26 +0900

    로그인 페이지 UI

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'mobx-react';
 import App from '~App';
 import RootStore from '~stores/RootStore';
+import './index.css';
 
 const rootStore = new RootStore();
 
--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,26 +1,73 @@
-import React from 'react';
+import React, { FormEvent } from 'react';
 import styled from 'styled-components';
-import Button from '~components/common/Button';
+import { Button, Form, Input, Icon } from 'antd';
 
 interface LoginProps {}
 
-const LoginBlock = styled.form`
-  .form-headline {
-  }
+const LoginBlock = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  overflow: hidden;
+  position: relative;
 
-  .form-head {
+  .container {
+    position: relative;
+    background-color: #ffffff;
+    width: 100%;
+    max-width: 400px;
+    padding: 16px;
+    height: auto;
+    overflow: hidden;
+    box-shadow: 0 3px 5px -1px rgba(0, 0, 0, 0.2),
+      0 5px 8px 0 rgba(0, 0, 0, 0.14), 0 1px 14px 0 rgba(0, 0, 0, 0.12);
+
+    .form-headline {
+      text-align: center;
+      font-size: 36px;
+      padding: 16px 32px;
+      color: #4a4a4a;
+    }
+
+    .login-button {
+      width: 100%;
+    }
   }
 `;
 
 const Login: React.SFC<LoginProps> = () => {
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+  };
+
   return (
     <LoginBlock>
-      <div className="form-headline">관리자 로그인</div>
-      <div className="form-head">
-        <form></form>
-      </div>
-      <div className="bottom">
-        <button>로그인</button>
+      <div className="container">
+        <div className="form-headline">관리자 로그인</div>
+        <Form onSubmit={handleSubmit}>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              placeholder="아이디"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Input
+              prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+              type="password"
+              placeholder="비밀번호"
+            />
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" className="login-button">
+              로그인
+            </Button>
+          </Form.Item>
+        </Form>
       </div>
     </LoginBlock>
   );

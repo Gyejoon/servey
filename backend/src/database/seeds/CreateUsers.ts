b6f3484e55711a3c24d888fe146421b10c2e176d
Jun Young Kang <00700mm@gmail.com>
2020-08-03 02:50:46 +0900

    fix: Seed => Seeder로 변경

--- backend/src/database/seeds/CreateUsers.ts
+++ backend/src/database/seeds/CreateUsers.ts
@@ -1,12 +1,10 @@
-import { Factory, Seed } from 'typeorm-seeding';
+import { Factory, Seeder } from 'typeorm-seeding';
 import { Connection } from 'typeorm/connection/Connection';
 
 import { User } from '../../../src/api/models/User';
 
-export class CreateUsers implements Seed {
-
-    public async seed(factory: Factory, connection: Connection): Promise<any> {
-        await factory(User)().seedMany(10);
-    }
-
+export class CreateUsers implements Seeder {
+  async run(factory: Factory, connection: Connection): Promise<void> {
+    await factory(User)().seedMany(10);
+  }
 }
